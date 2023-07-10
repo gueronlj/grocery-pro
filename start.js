1b697b6afd9bc06bd94947b1a95ecd03d25b072b
Lorens <gueronlj@gmail.com>
2023-07-10 18:08:23 -0400

    gets aldi special ad

--- start.js
+++ start.js
@@ -1,39 +1,33 @@
-const puppeteer = require('puppeteer');
+import puppeteer from "puppeteer";
 
-const ZIPCODE = '22041';
+const ZIPCODE = '22151';
 
-const scrape = async () => {
+const getAldiAd = async () => {
    const browser = await puppeteer.launch();
    const page = await browser.newPage();
+   await page.goto('https://stores.aldi.us/');
+   await page.setViewport({width: 1440, height: 900});
 
-   try{
-      await page.goto('https://stores.aldi.us/');
-      console.log(page.url());
+   // Type into search box
+   await page.waitForSelector('.search-input', {timeout: 10000});
+   await page.type('.search-input', ZIPCODE);
+   await page.keyboard.press('Enter');
 
-      await page.setViewport({width: 1440, height: 900});
+   // Wait and click on Store Details button
+   let selector = '.Teaser-link';
+   await page.waitForSelector(selector, {timeout: 10000});
+   const options = await page.$$eval(selector, options => {
+      return options.map(option => option.href);
+   });
+   const url = options[1];
+   await page.goto(url);
 
-      // Type into search box
-      await page.waitForSelector('.search-input', {timeout: 5000});
-      await page.type('.search-input', ZIPCODE);
-      await page.keyboard.press('Enter');
-      console.log('Typed the zipcode')
-
-      // Wait and click on Store Details button
-      let selector = '.Teaser-link';
-      await page.waitForSelector(selector, {timeout: 5000});
-      await page.click(selector);
-      console.log('clicked store details button');
-
-      const list = await page.evaluate(() => {
-         Array.from(document.querySelectorAll('a'));
-      })
-      console.log(list);
-
-   } catch(err) {
-      console.log(err);
-   }
-
-  await browser.close()
+   //Get URL for local store Ad.
+   selector = '.Hero-cta--secondary';
+   await page.waitForSelector(selector, {timeout: 10000});
+   const ALDI_AD = await page.$eval(selector, ele => ele.href)
+   console.log(ALDI_AD);
+   await browser.close();
 }
 
-scrape()
+getAldiAd()
